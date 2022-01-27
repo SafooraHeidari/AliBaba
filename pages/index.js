1da3f563ed1d8955bf6358b9cc5ea4849e5477d0
Safoora Heidari <safoora.heidari@yahoo.com>
2022-01-27 22:25:33 +0330

    SearchBox is here

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,26 @@
 import {Box, Container} from "@mui/material";
 import theme from "../styles/theme";
 import {ThemeProvider} from "@emotion/react";
+import SearchBox from "../Components/SearchBox/SearchBox";
 
 export default function Home() {
     return (
         <Box>
+            <ThemeProvider theme={theme}>
             <Box>
                 // NavBar
             </Box>
-            <ThemeProvider theme={theme}>
+
             <Container sx={{ backgroundColor: theme.palette.success.three }}>
              //body
+                <SearchBox/>
             </Container>
-            </ThemeProvider>
+
             <Box>
                 // pre-footer
                 // footer
             </Box>
+            </ThemeProvider>
         </Box>
     )
 }
