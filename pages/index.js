a6f88f336b0b12dbb57f71dad56b3e1de3f2eedb
qazal <qazalheydarpour@yahoo.com>
2022-01-30 19:31:22 +0330

    index changed

--- pages/index.js
+++ pages/index.js
@@ -2,30 +2,12 @@ import {Box, Container} from "@mui/material";
 import theme from "../styles/theme";
 import Footer from '../Components/Footer/Footer'
 import {ThemeProvider} from "@emotion/react";
-<<<<<<< HEAD
 import NavBar from "../Components/NavBar/NavBar";
 import RecentSearch from "../Components/RecentSearch/RecentSearch";
 import YourQuestions from "../Components/YourQuestions/YourQuestions";
 
 export default function Home() {
     return (
-
-=======
-import Advertise from "../Components/Advertise/Advertise";
-
-export default function Home() {
-    return (
-        <Box>
-            <Box>
-                // NavBar
-            </Box>
-            <ThemeProvider theme={theme}>
-            <Container sx={{ backgroundColor: theme.palette.success.three }}>
-             //body
-             <Advertise/>
-            </Container>
-            </ThemeProvider>
->>>>>>> advertise
             <Box>
                 <NavBar/>
                 <Container>
