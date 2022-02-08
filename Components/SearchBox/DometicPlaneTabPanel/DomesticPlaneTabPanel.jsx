36fb5cfa61355f87e07d6ddf1e620e7e338b8757
Safoora Heidari <safoora.heidari@yahoo.com>
2022-02-08 22:14:41 +0330

    Domestic Plane Panel created

--- Components/SearchBox/DometicPlaneTabPanel/DomesticPlaneTabPanel.jsx
+++ Components/SearchBox/DometicPlaneTabPanel/DomesticPlaneTabPanel.jsx
@@ -5,7 +5,7 @@ import {Box, Button, InputAdornment} from "@mui/material";
 import Hotel from "../../../styles/assest/Icons/Hotel";
 import DatePicker from "./MyDatePicker";
 import MyDatePicker from "./MyDatePicker";
-import FlightMode from "./FlightMode"
+import FlightMode2 from "./FlightMode2"
 
 const optionsCity = ['تهران', 'شیراز', 'مشهد'];
 const optionsNum = [1, 2, 3, 4];
@@ -16,7 +16,7 @@ const optionsNum = [1, 2, 3, 4];
 const sxFrom = {
         "& .css-ekiabt-MuiInputBase-root-MuiOutlinedInput-root":{
             borderRadius: '0 8px 8px 0',
-            height: '48px'
+            height: '48px',
         },
         width:'229px',
 }
@@ -30,19 +30,32 @@ const sxTo = {
     marginLeft: '21px'
 }
 
+const sxPassengers = {
+    "& .css-ekiabt-MuiInputBase-root-MuiOutlinedInput-root":{
+        borderRadius: '8px',
+        height: '48px'
+    },
+    width:'183px',
+    marginLeft: '21px'
+}
+
+
+
+
+
 
 
 function DomesticPlaneTabPanel(){
     return(
         <Box sx={{display:'flex',flexDirection:'column'}}>
 
-            <FlightMode/>
+            <FlightMode2/>
 
             <Box sx={{display:'flex',flexDirection:'row'}}>
                 <Autocomplete sx={sxFrom}
                               freeSolo
                               options={optionsCity}
-                              renderInput={(params) => (<TextField size="small" {...params} label="مبدا (شهر)"/>
+                              renderInput={(params) => (<TextField size="small" InputLabelProps={{style: {float: 'left'}}}  {...params} label="مبدا (شهر)"/>
                               )}
                 />
                 <Autocomplete sx={sxTo}
@@ -51,12 +64,12 @@ function DomesticPlaneTabPanel(){
                               renderInput={(params) => <TextField size="small" {...params} label="مقصد (شهر)" />}
                 />
                 <MyDatePicker/>
-                <Autocomplete sx={{width:'15%'}}
+                <Autocomplete sx={sxPassengers}
                               freeSolo
                               options={optionsNum}
-                              renderInput={(params) => <TextField {...params} label="مسافران" />}
+                              renderInput={(params) => <TextField size="small" {...params} label="مسافران" />}
                 />
-                <Button variant="contained" sx={{width:'183px', height:'48px'}}>جستجو</Button>
+                <Button variant="contained" sx={{width:'183px', height:'45px'}}>جستجو</Button>
 
             </Box>
 
