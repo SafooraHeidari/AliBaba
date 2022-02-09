e9a651edc1cd89d55493b4085763933158e303d3
Safoora Heidari <safoora.heidari@yahoo.com>
2022-02-09 09:42:50 +0330

    index.js changed

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import { Box, Container } from "@mui/material";
 import theme from "../styles/theme";
 import {ThemeProvider} from "@emotion/react";
+import YourQuestions from "./../Components/YourQuestions/YourQuestions"
 
 export default function Home() {
   return (
     <ThemeProvider theme={theme}>
       <Box>
 
-          <NavBar />
-          <RecentSearch/>
+          {/*<NavBar />*/}
+          <YourQuestions/>
 
         <Box>// pre-footer // footer</Box>
       </Box>
