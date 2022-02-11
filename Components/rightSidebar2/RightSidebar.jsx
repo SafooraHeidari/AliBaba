ac511222b678a77ec214935bfa8b8a8e6a4e1567
qazal <qazalheydarpour@yahoo.com>
2022-02-11 18:45:27 +0330

    RightsideBar2

--- Components/rightSidebar2/RightSidebar.jsx
+++ Components/rightSidebar2/RightSidebar.jsx
@@ -4,27 +4,34 @@ import AccordionSummary from "@mui/material/AccordionSummary";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import theme from "../../styles/theme";
 import {Accordion, AccordionDetails, Typography} from "@mui/material";
-import TypeSidebar from "./TypeSidebar";
+import TypeSidebar from "./ChekboxSidebar";
+import TimeSlider from "./TimeSlidebar";
 const AccordionData=[
     {
         id:1,
         title:'ساعت حرکت',
+        childe:<TimeSlider/>
+
     },
     {
         id:2,
         title:'نوع بلیط',
+        childe:<TypeSidebar first={'چارتر'} second={'سیستمی'}/>
     },
     {
         id:3,
         title:'کلاس پروازی',
+        childe:<TypeSidebar first={'اکونومی'} second={'بیزینس'}/>
     },
     {
         id:4,
         title:'شرکت‌های هواپیمایی',
+        childe:<TypeSidebar first={'چارتر'} second={'سیستمی'}/>
     },
     {
         id:5,
         title:'موارد دیگر',
+        childe:<TypeSidebar first={'نمایش بلیط‌های تکراری'} second={'نمایش بلیط‌های موجود'}/>
     },
 ]
 const styles = {
@@ -42,9 +49,15 @@ export default function RightSidebar() {
     };
     return(
         <>
+        <Accordion sx={{width:'30%'}} style={styles.myAccordion}>
+                    <AccordionSummary
+                        id="panel1bh-header">
+                        <Typography sx={{fontFamily:'AliBaba',color: theme.palette.gray.five,flexShrink: 0,paddingY:'.8rem',fontSize:'.875rem',fontWeight: '500'}}>نتایج:13</Typography>
+                    </AccordionSummary>
+                </Accordion>
         {
             AccordionData.map( data => (
-                <Accordion style={styles.myAccordion} expanded={expanded === `panel${data.id}`} onChange={handleChange(`panel${data.id}`)}>
+                <Accordion sx={{width:'30%'}} style={styles.myAccordion} expanded={expanded === `panel${data.id}`} onChange={handleChange(`panel${data.id}`)}>
                     <AccordionSummary
                         expandIcon={<ExpandMoreIcon/>}
                         aria-controls="panel1bh-content"
@@ -52,11 +65,12 @@ export default function RightSidebar() {
                         <Typography sx={{fontFamily:'AliBaba',color: theme.palette.gray.five,flexShrink: 0,paddingY:'.8rem',fontSize:'.875rem',fontWeight: '500'}}>{data.title}</Typography>
                     </AccordionSummary>
                     <AccordionDetails>
-                        <Typography sx={{fontFamily:'AliBaba',color:theme.palette.gray.four,fontSize:'1rem',lineHeight:'36px',marginRight:'3.2rem'}}><TypeSidebar/></Typography>
+                        <Typography sx={{fontFamily:'AliBaba',color:theme.palette.gray.four,fontSize:'1rem',lineHeight:'36px',marginRight:'3.2rem',marginRight:'10px',marginLeft:'20px'}}>{data.childe}</Typography>
                     </AccordionDetails>
                 </Accordion>
             ))
         }
+
         
         </>
     )
